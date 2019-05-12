220f92d6f2bc389ab751278f9cc417c84a344d0a
Eun Gang Ku <kegang@naver.com>
2019-05-12 16:14:00 +0900

    Update extract-files.sh

--- leeco/s2/extract-files.sh
+++ leeco/s2/extract-files.sh
@@ -25,9 +25,9 @@ VENDOR=leeco
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-LINEAGE_ROOT="$MY_DIR"/../../..
+rr_ROOT="$MY_DIR"/../../..
 
-HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
+HELPER="$rr_ROOT"/vendor/rr/build/tools/extract_utils.sh
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
@@ -51,7 +51,7 @@ else
 fi
 
 # Initialize the helper
-setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT"
+setup_vendor "$DEVICE" "$VENDOR" "$rr_ROOT"
 
 extract "$MY_DIR"/proprietary-files.txt "$SRC"
 
